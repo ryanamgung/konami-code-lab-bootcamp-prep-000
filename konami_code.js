b45ad394d2ac4cb64f1060c652f8e740611be7b3
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-22 21:29:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,9 +12,10 @@ const codes = [
 ];
 
 function init() {
+  let index = 0;
+
   document.body.addEventListener('keydown', function (e) {
     const key = e.key;
-    let index = 0;
 
     if (key === codes[index]) {
       console.log(key)
